e1ae2def10b2639f4b5fa9894ca0e3d34b01a149
naim-007 <naim1823007@gmail.stud.kuet.ac.bd>
2020-11-17 01:28:42 +0600

    first two portion done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React, { createContext, useState } from 'react';
 import './App.css';
+import { BrowserRouter, Route, Router } from 'react-router-dom';
+import Navigation from './Components/Navigation/Navigation';
+import Home from './Components/HomePage/Home/Home';
+import HotelNavigation from './Components/HotelNavigation/HotelNavigation';
+import HotelList from './Components/HotelList/HotelList';
+export const ClientContext = createContext()
 
 function App() {
+  const [clientsNeed, setClientsNeed] = useState({})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ClientContext.Provider value={[clientsNeed, setClientsNeed]}>
+      <BrowserRouter>
+        <Route exact path='/' component={Navigation} />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/hotelbooking' component={HotelNavigation} />
+        <Route exact path='/hotelbooking' component={HotelList} />
+      </BrowserRouter>
+    </ClientContext.Provider>
   );
 }
 
